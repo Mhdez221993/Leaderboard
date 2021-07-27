1bbd93348fccf9b66c99da328a876bf9f2226f6c
Moises <mhdezcoronado@gmail.com>
2021-07-27 20:08:58 +0200

    fix error on test file

--- __test__/list.test.js
+++ __test__/list.test.js
@@ -1,18 +1,20 @@
 const List = require('../src/components/list');
 
 
-
 describe('List Class', () => {
-  let score =  [ {"name": "Jane", "score": 25}, {"name": "John", "score": 40}, {"name": "Jennifer", "score": 20}]
-  let sortedScore =  [{"name": "Jennifer", "score": 20}, {"name": "Jane", "score": 25}, {"name": "John", "score": 40}]
-  let newList = new List(score)
-  let ul = '<ul> <li>Jennifer: 20</li>' + '\n' + '<li>Jane: 25</li>' + '\n' + '<li>John: 40</li> </ul>'
+  const score = [{ name: 'Jane', score: 25 }, { name: 'John', score: 40 }, { name: 'Jennifer', score: 20 }];
+  const sortedScore = [{ name: 'Jennifer', score: 20 }, { name: 'Jane', score: 25 }, { name: 'John', score: 40 }];
+  const newList = new List(score);
+
+  const ul = `<ul> <li>Jennifer: 20</li>
+<li>Jane: 25</li>
+<li>John: 40</li> </ul>`;
 
   test('sort the scores arry base on the score property', () => {
-    expect( newList.sortScore() ).toEqual(sortedScore)
+    expect(newList.sortScore()).toEqual(sortedScore);
   });
 
   test('', () => {
-    expect( newList.createMarkup(sortedScore) ).toBe(ul)
+    expect(newList.createMarkup(sortedScore)).toBe(ul);
   });
-})
+});
