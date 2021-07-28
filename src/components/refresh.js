24de9cfb9516e4b5b5ae77757f5a91f20f8e0fcc
Moises <mhdezcoronado@gmail.com>
2021-07-28 18:22:04 +0200

    Add event to refresh button

---
+++ src/components/refresh.js
@@ -0,0 +1,29 @@
+class RefreshButton {
+  constructor(users){
+    this.users = users
+  }
+
+  createMarkup(){
+    return `
+    <h2>Recent score</h2>
+    <button id="refresh" class="refresh"> Refresh <i class="fas fa-sync-alt"></i> </button>
+    `
+  }
+
+  render(selector) {
+    const markup = this.createMarkup();
+    const parent = document.getElementById(selector);
+
+    parent.innerHTML = markup;
+  }
+
+  bindEvents() {
+    const refresh = document.getElementById("refresh");
+    refresh.addEventListener("click", e => {
+      e.preventDefault();
+      this.users.getScore()
+    });
+  }
+}
+
+module.exports = RefreshButton;
